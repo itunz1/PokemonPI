3cb73ac89a4f1d9eaaf338b540c91bdac178ed8e
itunz1 <stx_jose@hotmail.com>
2023-02-02 18:33:49 -0400

    dbfix

--- api/src/db.js
+++ api/src/db.js
@@ -6,39 +6,39 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT,
 } = process.env;
 
-// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, {
-//   logging: false, // set to console.log to see the raw SQL queries
-//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-// });
+const sequelize = new Sequelize(`postgres://postgres:17227936@rds.cb75r7pyujoj.us-east-1.rds.amazonaws.com/pokemon`, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+});
 
-let sequelize =
-  process.env.NODE_ENV === "production"
-    ? new Sequelize({
-        database: 'pokemon',
-        dialect: "postgres",
-        host: 'rds.cb75r7pyujoj.us-east-1.rds.amazonaws.com',
-        port: '5432',
-        username: 'postgres',
-        password: '17227936',
-        pool: {
-          max: 3,
-          min: 1,
-          idle: 10000,
-        },
-        dialectOptions: {
-          ssl: {
-            require: true,
-            // Ref.: https://github.com/brianc/node-postgres/issues/2009
-            rejectUnauthorized: false,
-          },
-          keepAlive: true,
-        },
-        ssl: true,
-      })
-    : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-        { logging: false, native: false }
-      );
+// let sequelize =
+//   process.env.NODE_ENV === "production"
+//     ? new Sequelize({
+//         database: ,
+//         dialect: ,
+//         host: 'rds.cb75r7pyujoj.us-east-1.rds.amazonaws.com',
+//         port: '5432',
+//         username: 'postgres',
+//         password: '',
+//         pool: {
+//           max: 3,
+//           min: 1,
+//           idle: 10000,
+//         },
+//         dialectOptions: {
+//           ssl: {
+//             require: true,
+//             // Ref.: https://github.com/brianc/node-postgres/issues/2009
+//             rejectUnauthorized: false,
+//           },
+//           keepAlive: true,
+//         },
+//         ssl: true,
+//       })
+//     : new Sequelize(
+//         `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+//         { logging: false, native: false }
+//       );
 
 const basename = path.basename(__filename);
 
